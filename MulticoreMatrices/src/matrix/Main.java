7928bf6d20223e417be925e42e67f024edfed703
Cody157 <codyestes157@gmail.com>
2014-11-13 06:57:53 -0600

    Added functionality for computing an LU decomposition.
    I wrote two methods that do this. In the regular one, LU(), a
    decomposition is only valid if the matrix is square and non-singular. If
    permutations are required, it further returns a P matrix to permute the
    LU decomposition.
    
    In the second method, LUextended, I've relaxed the requirements of an LU
    decomposition to allow finding a factorization for any matrix, since
    such factorizations are useful and I don't really want to see that one
    can't be found when in reality it can.
    
    These methods are not extensively tested, so if you find some cases
    where either doesn't work please let me know. The first can be checked
    against something like Wolfram Alpha or Mathematica for results, I just
    haven't tested for corner cases. The second one... basically you have to
    check by hand, which may be hard if you don't know the underlying
    algorithm! For some reason, neither Wolfram nor Mathematica allow
    extending the LU decomposition as I did. They do exactly what the first
    one is, where square, non-singular matrices return an L, U, and possibly
    a P. They don't allow for rectangular or singular matrices, though the
    algorithm is easily extended to include them (as I did).

--- MulticoreMatrices/src/matrix/Main.java
+++ MulticoreMatrices/src/matrix/Main.java
@@ -15,18 +15,28 @@ public class Main
 			for (int j = 0; j < 3; j++)
 			{ 
 				mat[i][j] = (double) i + j;
-				if (i == 0 && j == 2) mat[i][j]++;
+				if (i == 0 && j == 2)
+					mat[i][j]++;
 			}
 		}
 		
-		Matrix matrix = Matrix.randomMatrix(3, 3, 0, 10);
-		System.out.println(matrix.toString(3));
+//		Matrix matrix = Matrix.randomMatrix(3, 3, 0, 10);
+		Matrix matrix = new Matrix(mat);
+		System.out.println("matrix\n" + matrix.toString(3));
 		Matrix rref = matrix.rref();
-		System.out.println(rref.toString(3));
+		System.out.println("rref\n" + rref.toString(3));
 		Matrix inverse = matrix.inverse();
-		System.out.println(inverse.toString(3));
+		if (inverse != null)
+			System.out.println("inverse\n" + inverse.toString(3));
+		else
+			System.out.println("Not invertible\n");
 		Double determinant = matrix.determinant();
-		System.out.println(determinant);
+		System.out.println("Determinant\n" + determinant);
+		Matrix[] LU = matrix.LU();
+		System.out.println("L\n" + LU[0].toString(3));
+		System.out.println("U\n" + LU[1].toString(3));
+		if (LU.length == 3)
+			System.out.println("P\n" + LU[2].toString(3));
 	}
 
 }
